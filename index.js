6e3ca2858e126aed521c5430014e5c1d29590a2f
takanorip <takanori0170@gmail.com>
2021-09-14 00:47:59 +0900

    fix: remove unused files

--- index.js
+++
@@ -1,8 +0,0 @@
-const searchGithub = (keyword) => {
-  const api = "https://api.github.com/search/repositories"
-  fetch(api + 'q=topic:ruby+topic:rails')
-    .then(response => response.json())
-    .then(data => console.log(data));
-}
-
-searchGithub()
